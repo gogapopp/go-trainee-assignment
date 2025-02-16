1a4c93c3a68856f061a6295a448fb59b97bd8032
German <gogapopp@gmail.com>
2025-02-16 03:58:32 +0300

    fix: lint and tests

--- internal/repository/postgres/integration/postgres_test.go
+++ internal/repository/postgres/integration/postgres_test.go
@@ -2,15 +2,24 @@
 package intergration
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/gogapopp/go-trainee-assignment/internal/libs/config"
 )
 
 func getDSN(t *testing.T) string {
-	cfg, err := config.New("../../../../.env")
+	envPath := "../../../../.env"
+
+	if ws := os.Getenv("GITHUB_WORKSPACE"); ws != "" {
+		envPath = filepath.Join(ws, ".env")
+	}
+
+	cfg, err := config.New(envPath)
 	if err != nil {
 		t.Fatalf("failed to load config: %v", err)
 	}
+
 	return cfg.PGConfig.DSN
 }
