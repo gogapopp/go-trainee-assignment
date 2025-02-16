f9b293ebb851244d24d8b5db1fa13b267bb9cc6b
German <gogapopp@gmail.com>
2025-02-16 04:07:39 +0300

    fix: ci and tests

--- internal/repository/postgres/integration/postgres_test.go
+++ internal/repository/postgres/integration/postgres_test.go
@@ -2,6 +2,7 @@
 package intergration
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -10,16 +11,32 @@ import (
 )
 
 func getDSN(t *testing.T) string {
-	envPath := "../../../../.env"
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
 
-	if ws := os.Getenv("GITHUB_WORKSPACE"); ws != "" {
-		envPath = filepath.Join(ws, ".env")
+	if user == "" || password == "" || dbName == "" || host == "" || port == "" {
+		envPath := "../../../../.env"
+
+		if ws := os.Getenv("GITHUB_WORKSPACE"); ws != "" {
+			envPath = filepath.Join(ws, ".env")
+		}
+
+		cfg, err := config.New(envPath)
+		if err != nil {
+			t.Fatalf("failed to load config: %v", err)
+		}
+
+		return cfg.PGConfig.DSN
 	}
-
-	cfg, err := config.New(envPath)
-	if err != nil {
-		t.Fatalf("failed to load config: %v", err)
-	}
-
-	return cfg.PGConfig.DSN
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv(user),
+		os.Getenv(password),
+		os.Getenv(host),
+		os.Getenv(port),
+		os.Getenv(dbName),
+	)
 }
